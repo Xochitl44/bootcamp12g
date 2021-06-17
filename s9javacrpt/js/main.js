abe40462246f54e8026299b4385136f337d579ca
XochitlMoreno <xochitlmf44@outlook.com>
2021-06-16 23:58:08 -0500

    outside-class exercises

---
+++ s9javacrpt/js/main.js
@@ -0,0 +1,32 @@
+//ejercicio 1pedir 2 números al usuario y mostrar en consola el resultado de aplicar todas las operaciones aritméticas entre esos dos números
+/*
+var number1 = parseInt(prompt("ingresa un numero"))
+var number2 = parseInt(prompt("ingresa un segundo numero"))
+
+console.log(number1 + number2)
+console.log(number1 - number2)
+console.log(number1 * number2)
+console.log(number1 / number2)
+console.log(number1 % number2)
+*/
+
+
+//ejercicio 2: pedir al usuario dos números y escribir en consola si el primer número es mayor que el segundo
+/*
+    var number1 = parseInt(prompt("ingresa un numero"))
+    var number2 = parseInt(prompt("ingresa un segundo numero"))
+
+    console.log(number1>number2)
+*/
+
+
+//ejercicio 3: pedir al usuario dos letras, y determinar si la primera aparece antes o después en el alfabeto
+
+var letter1 = prompt("ingresa una letra")
+var letter2 = prompt("ingresa otra letra")
+
+console.log(letter1>letter2)
+
+//parseInt() : Analiza hasta el primer no-digito y devuelve lo que haya analizado.
+//Number(): convierte toda la cadena en un n’umero, que también puede ser un float.
+    
